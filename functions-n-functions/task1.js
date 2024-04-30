927af30b7ec289919deed47fbaaa0aeefa55f37a
guijun13 <guilhermejun13.grillo@gmail.com>
2024-04-30 09:52:52 -0300

    fix: changed data format for task1/hw3

--- functions-n-functions/task1.js
+++ functions-n-functions/task1.js
@@ -1,20 +1,23 @@
-const productsList = [1000, 1500, 500, 800];
+const productsList = [
+  { name: 'Product 1', price: 75 },
+  { name: 'Product 2', price: 150 },
+];
 
 function calculateDiscountedPrice(productsList, discountPercentage) {
-  const discountedProductsList = productsList.map((product) => {
-    return product - (product * discountPercentage) / 100;
+  const discountedProducts = productsList.map((product) => {
+    const discountedPrice = product.price - (product.price * discountPercentage) / 100;
+    return { ...product, price: discountedPrice };
   });
-
-  return discountedProductsList;
+  return discountedProducts;
 }
 
 function calculateTotalPrice(productsList) {
   const totalPrice = productsList.reduce((total, product) => {
-    return total + product;
+    return total + product.price;
   }, 0);
 
   return totalPrice;
 }
 
 console.log(calculateDiscountedPrice(productsList, 10));
-console.log('The total price is: ', calculateTotalPrice(productsList));
+console.log('The total price is:', calculateTotalPrice(productsList));
